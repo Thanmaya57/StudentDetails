a336b908d301e26a6b6ec1dd89d5aa200fabd562
Thanmaya57 <134828674+Thanmaya57@users.noreply.github.com>
2023-05-28 11:32:50 +0530

    Create SpringwebsqlApplication.java

---
+++ SpringwebsqlApplication.java
@@ -0,0 +1,12 @@
+package com.example.springloginform;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebsqlApplication.class, args);
+	}
+
+}
